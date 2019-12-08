959af817285e1c653d0dc561ff0a54ac1f9b9872
agdavid <antonio.g.david@gmail.com>
2019-12-08 11:54:15 -0500

    loadData function to retrieve JSON store

--- utils.js
+++ utils.js
@@ -1,6 +1,20 @@
+const fs = require('fs');
+
 const addKeyValue = (key, value) => {
-  console.log(key);
-  console.log(value);
+  const data = loadData();
+  console.log(data);
+};
+
+const loadData = () => {
+  try {
+    // locate file and return array of objects
+    let dataBuffer = fs.readFileSync('data.json');
+    let dataJSON = dataBuffer.toString();
+    return JSON.parse(dataJSON);
+  } catch (e) {
+    // no such file located and return empty array
+    return [];
+  }
 };
 
 module.exports = {
